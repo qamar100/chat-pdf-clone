a677bdfa43d8144a5c53cc1543c706f198f094a1
qamar100 <qamarabbas.qa549@gmail.com>
2024-05-10 11:57:24 +0500

    first commit

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -10,6 +10,8 @@ export const chats = pgTable('chats', {
     fileKey: text('file_key').notNull(),
 });
 
+export type DrizzleChat = typeof chats.$inferSelect;  //to export all the above objects to sidebar
+
 export const messages = pgTable('messages', {
     id: serial('id').primaryKey(),
     chatId: integer('chat_id').references(() => chats.id).notNull(),
