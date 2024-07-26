7ec6c92ee0efef3811a0f997e543cf110d8630ef
qamar100 <qamarabbas.qa549@gmail.com>
2024-07-26 23:23:08 +0500

    Closes #2

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,6 +1,7 @@
-import { Pinecone } from "@pinecone-database/pinecone";
+import { Index, Pinecone } from "@pinecone-database/pinecone";
 import { convertToAscci } from "./utils";
 import { getEmbedding } from './embedding';
+import { index } from "drizzle-orm/mysql-core";
 
 export async function getMatchesFromEmbeddings(
   embeddings: number[],
@@ -12,14 +13,15 @@ export async function getMatchesFromEmbeddings(
     });
     const pineconeIndex = await client.index("chatpdf");
     const namespace = pineconeIndex.namespace(convertToAscci(fileKey));
-    const queryResult = await namespace.query({
+    const queryResult = await pineconeIndex.query({
       topK: 5,
       vector: embeddings,
       includeMetadata: true,
     });
     return queryResult.matches || [];
+
   } catch (error) {
-    console.log("error querying embeddings", error);
+  //  console.log("error querying embeddings", error);
     throw error;
   }
 }
@@ -28,10 +30,10 @@ export async function getContext(query: string, fileKey: string) {
   const queryEmbeddings = await getEmbedding(query);
   //console.log("Query Embeddings:", queryEmbeddings); //debugging line
   const matches = await getMatchesFromEmbeddings(queryEmbeddings, fileKey);
-  console.log("Matches:", matches); //debugging line
+  //console.log("Matches:", matches); //debugging line
   const qualifyingDocs = matches.filter(
-    (match) => match.score && match.score > 0.9
-  );
+    (match) => match.score && match.score > 0.7  // less then 70% the matches will probably be irrelevant qeury
+  );                                             // the score here tells the relevance between user query and the retreived context if its less than 70% then it will be filtered earlier i set it to 90% but the score of relavance i was getting was somewhere between in 80s that i was i was not getting any context
 
   console.log("Qualifying Docs:", qualifyingDocs);
 
@@ -42,8 +44,8 @@ export async function getContext(query: string, fileKey: string) {
 
   let docs = qualifyingDocs.map((match) => (match.metadata as Metadata).text);
   const context = docs.join("\n").substring(0, 3000);
-  console.log("Context:", context); // debugg
-  console.log("Docs:", docs);
+  //console.log("Context:", context); // debugg
+  //console.log("Docs:", docs);
   return context;
   
 }
